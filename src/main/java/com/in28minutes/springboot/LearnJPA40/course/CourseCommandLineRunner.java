31a7d87393e956fc5227343c0e0d5ac260de73e8
WesBourkeMusic <wesbourkeavl@gmail.com>
2023-02-13 23:36:12 -0500

    Learned about spring data jpa.

--- src/main/java/com/in28minutes/springboot/LearnJPA40/course/CourseCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/LearnJPA40/course/CourseCommandLineRunner.java
@@ -12,17 +12,28 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS now!", "in28minutes"));
-        repository.insert(new Course(2, "Learn Azure now!", "in28minutes"));
-        repository.insert(new Course(3, "Learn DevOps now!", "in28minutes"));
+        repository.save(new Course(2, "Learn Azure now!", "in28minutes"));
+        repository.save(new Course(1, "Learn AWS now!", "in28minutes"));
+        repository.save(new Course(3, "Learn DevOps now!", "in28minutes"));
 
-        repository.deleteById(1);
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        repository.deleteById(1l);
+        System.out.println(repository.findById(2l));
+        System.out.println(repository.findById(3l));
+
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
+
+        System.out.println(repository.findByAuthor("in28minutes"));
+        System.out.println(repository.findByName("Learn DevOps now!"));
+
+
     }
 }
---
+++ src/main/java/com/in28minutes/springboot/LearnJPA40/course/CourseSpringDataJpaRepository.java
@@ -0,0 +1,9 @@
+package com.in28minutes.springboot.LearnJPA40.course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+    List<Course>findByAuthor(String author);
+    List<Course>findByName(String name);
+}
